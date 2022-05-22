09ee2a8c67f89f9dcac9aa18d23c879b34d3be0c
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-05-21 21:13:44 -0600

    added CoderStamp function and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.png";
 import "./App.css";
+import CoderStamp from "./CoderStamp.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <img src={logo} className="app-logo img-fluid" alt="logo" />
+
+      <CoderStamp />
     </div>
   );
 }
---
+++ src/CoderStamp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./CoderStamp.css";
+
+export default function CoderStamp() {
+  return (
+    <footer className="footer">
+      This project was created by Kim MacVicar and is{" "}
+      <a
+        className="link"
+        href="https://github.com/KimMacVicar/dictionary-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open-sourced on GitHub
+      </a>{" "}
+      and{" "}
+      <a
+        className="link"
+        href="https://elegant-crisp-fa71cf.netlify.app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        hosted on Netlify
+      </a>
+    </footer>
+  );
+}
